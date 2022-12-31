b01986c8d6531566cea0a217e0da024326011186
Anthony DOMEON <anthony.domeon@etu.univ-nantes.fr>
2022-12-31 02:51:17 +0100

    Deuxieme version de l'ouverture de la popup pour n'afficher que ce qui n'est pas déja afficher

--- src/views/Answer.vue
+++ src/views/Answer.vue
@@ -2,21 +2,23 @@
     <ion-item @touchstart=touchOnHold @touchend=simpleTouch>
         <ion-radio slot=start :value=answerValue @ionFocus="$emit('updatePick', answerValue)" />
         <ion-label class="ion-text-wrap" :for="'response' + answerValue">
-            {{ content.length > MAX_LENGTH_POPOVER
-        ? content.slice(0, MAX_LENGTH_POPOVER).replace(/ \S*$/, '[...]') : content
+            {{ isCrop && indexOfSplit < content.split(" ").length - 1
+        ? content.split(" ").slice(0, indexOfSplit).join(" ") + '[...]' : content
 }}
         </ion-label> <br>
     </ion-item>
     <ion-popover :is-open="isOpen" @didDismiss="isOpen = false" :event="event" size="auto">
-        <ion-content class="ion-padding">{{ content }}</ion-content>
-    </ion-popover>
+                <ion-content class="ion-padding">{{ '...' + content.split(" ").slice(indexOfSplit,
+        content.length).join(" ")
+                    }}</ion-content>
+                </ion-popover>
 </template>
   
 <script lang="ts">
 import { IonItem, IonLabel, IonRadio, IonPopover, IonContent } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
-const MAX_LENGTH_POPOVER = 85
+const MAX_LENGTH_POPOVER = 110
 
 export default defineComponent({
     name: 'Answer',
@@ -37,13 +39,30 @@ export default defineComponent({
             MAX_LENGTH_POPOVER,
             isOpen: false,
             event: null,
-            timer: 0
+            timer: 0,
+            indexOfSplit: 0,
+            isCrop: this.content.length > MAX_LENGTH_POPOVER
         }
+    },
+    created() {
+        if (this.isCrop) {
+            let countCaract = 0
+            this.content.split(" ").every((word, index) => {
+                if (index == this.content.split(" ").length - 1 || countCaract > MAX_LENGTH_POPOVER) {
+                    this.indexOfSplit = index
+                    return false
+                }
+                countCaract += word.length + 1
+                return true
+            })
+
+        }
+
     },
     methods: {
         touchOnHold(e: Event) {
             this.timer = setTimeout(() => {
-                if (this.content.length > MAX_LENGTH_POPOVER) {
+                if (this.isCrop && this.indexOfSplit < this.content.split(" ").length - 1) {
                     //@ts-ignore
                     this.event = e
                     this.isOpen = true
@@ -68,6 +87,7 @@ ion-item {
 }
 
 ion-popover {
-    --offset-x: 50px
+    --offset-x: 50px;
+    --offset-y: 25px;
 }
 </style>
--- src/views/QuestionAnswers.vue
+++ src/views/QuestionAnswers.vue
@@ -1,21 +1,21 @@
 <template>
     <div id=main-content>
         <ion-text>
-            <h2><a @click="canOpenPopover($event)">{{ id }} -
-                    {{ content.length > MAX_LENGTH_POPOVER
-        ? content.slice(0, MAX_LENGTH_POPOVER).replace(/ \S*$/, '[...]') : content
-}}
-                </a></h2>
+            <h2><a @click="canOpenPopover($event)">{{ id }} - {{ isCrop && indexOfSplit <
+        content.split(" ").length - 1 ? content.split(" ").slice(0, indexOfSplit).join(" ") + '[...]' : content
+}}</a></h2>
         </ion-text>
         <ion-popover :is-open="isOpen" :event="event" @didDismiss="isOpen = false" size="cover">
-            <ion-content class="ion-padding">{{ content }}</ion-content>
-        </ion-popover>
-        <ion-radio-group>
-            <ion-list lines="none">
-                <Answer v-for="(answer, index) in answers" :key="index" :answerValue="id + '-' + (index + 1)"
-                    :content="answer" @update-pick=udpatePick />
-            </ion-list>
-        </ion-radio-group>
+                        <ion-content class="ion-padding">{{ '...' + content.split(" ").slice(indexOfSplit,
+        content.length).join(" ")
+}}</ion-content>
+                        </ion-popover>
+                        <ion-radio-group>
+                            <ion-list lines="none">
+                                <Answer v-for="(answer, index) in answers" :key="index"
+                                    :answerValue="id + '-' + (index + 1)" :content="answer" @update-pick=udpatePick />
+                            </ion-list>
+                        </ion-radio-group>
     </div>
 
 </template>
@@ -26,7 +26,7 @@ import { IonText, IonList, IonRadioGroup, IonPopover, IonContent } from '@ionic/
 import Answer from './Answer.vue';
 import { dynamicResponses, questionsAnswers } from '../common';
 
-const MAX_LENGTH_POPOVER = 85
+const MAX_LENGTH_POPOVER = 90
 
 export default defineComponent({
     name: 'QuestionAnswers',
@@ -53,8 +53,24 @@ export default defineComponent({
             MAX_LENGTH_POPOVER,
             // Cette objet fait reference a la constant 'dynamicResponses' du fichier common, c'est un Proxy
             dynamicResponses,
+            indexOfSplit: 0,
             isOpen: false,
             event: null,
+            isCrop: this.content.length > MAX_LENGTH_POPOVER
+        }
+    },
+    created() {
+        if (this.isCrop) {
+            let countCaract = 0
+            this.content.split(" ").every((word, index) => {
+                if (index == this.content.split(" ").length - 1 || countCaract > MAX_LENGTH_POPOVER) {
+                    this.indexOfSplit = index
+                    return false
+                }
+                countCaract += word.length + 1
+                return true
+            })
+
         }
     },
     methods: {
@@ -77,7 +93,7 @@ export default defineComponent({
             }
         },
         canOpenPopover(e: Event) {
-            if (this.content.length > MAX_LENGTH_POPOVER) {
+            if (this.isCrop && this.indexOfSplit < this.content.split(" ").length - 1) {
                 //@ts-ignore
                 this.event = e
                 this.isOpen = true
@@ -99,10 +115,6 @@ div#main-content {
         a {
             color: inherit;
         }
-    }
-
-    ion-popover {
-        width: 80%;
     }
 
     ion-list {
